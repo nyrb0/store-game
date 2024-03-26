f660372f9c572a32e50a3da3c838794691356d06
Nurbolot <nubolotkadyrov06@gmail.com>
2024-03-27 01:35:21 +0600

    changed order-page

--- src/components/order-item/OrderItem.jsx
+++ src/components/order-item/OrderItem.jsx
@@ -1,11 +1,19 @@
 import { useDispatch } from 'react-redux';
 import orderItemClass from './OrderItem.module.scss'
 import GameCover from '../game-cover/GameCover'
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import {delateItemFromCart} from '../../redux/cart/reducer'
 const OrderItem = ({game})=>{
 
       const dispatch = useDispatch();
       // const handle
 
+      const handlerClick =()=>{
+
+            return(
+                  dispatch(delateItemFromCart(game.id))
+            )
+      }
       return(
             <div className={orderItemClass.item}>
                   <div className={orderItemClass.cover}>
@@ -16,6 +24,11 @@ const OrderItem = ({game})=>{
                   </div>
                   <div className={orderItemClass.price}>
                         <span>{game.price} сом</span>
+                        <AiOutlineCloseCircle
+                        size={25}
+                        className={orderItemClass.delateIcon}
+                        onClick={handlerClick}
+                        />
                   </div>
             </div>
       )
